a2d87be54b496ced3e7cb1ba772799bbdaf86f7e
nightblade9 <nightbladecodes@gmail.com>
2018-07-07 20:52:38 -0400

    Collision set up but doesn't work properly Bush collision shape seems off

--- source/Tilesets/Girl.gd
+++ source/Tilesets/Girl.gd
@@ -1,24 +1,24 @@
 extends Node2D
 
-var speed = 100
+var speed = 200
 var vel = Vector2()
 
 
 func _ready():
 	set_process(true)
 
-func _process(delta):
+func _physics_process(delta):
 	var velocity = Vector2(0, 0)
 	
 	if Input.is_key_pressed(KEY_RIGHT):
-		velocity.x = velocity.x + speed		
+		velocity.x = 1
 	elif Input.is_key_pressed(KEY_LEFT):
-		velocity.x = velocity.x - speed
+		velocity.x = -1
 	
 	if Input.is_key_pressed(KEY_UP):
-		velocity.y = velocity.y - speed
+		velocity.y = -1
 	elif Input.is_key_pressed(KEY_DOWN):
-		velocity.y = velocity.y + speed
+		velocity.y = 1
 	
-	self.position.x = self.position.x + (velocity.x * delta)
-	self.position.y = self.position.y + (velocity.y * delta)
+	velocity = velocity.normalized() * speed
+	move_and_slide(velocity)
