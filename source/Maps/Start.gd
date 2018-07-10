39ead39107590c035664326f249c2796c68e9bbe
nightblade9 <nightbladecodes@gmail.com>
2018-07-09 23:46:44 -0400

    Four meadows.

--- source/Maps/Start.gd
+++ source/Maps/Start.gd
@@ -2,7 +2,10 @@ extends Node2D
 
 var current_map
 var maps = {
-	"meadow1": "res://Maps/Overworld/Meadow1.tscn"
+	"meadow1": "res://Maps/Overworld/Meadow1.tscn",
+	"meadow2": "res://Maps/Overworld/Meadow2.tscn",
+	"meadow3": "res://Maps/Overworld/Meadow3.tscn",
+	"meadow4": "res://Maps/Overworld/Meadow4.tscn"
 }
 const STARTING_MAP = "meadow1"
 
@@ -11,11 +14,10 @@ const STARTING_MAP = "meadow1"
 # var b = "textvar"
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	self.current_map = load(maps[STARTING_MAP]).instance()
 	self.current_map.z_index = -1 # draw under the player
 	self.add_child(self.current_map)
+	# TODO: center player
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
