2e51074d416120e59ae1575c5e5f5bc380d1df11
nightblade9 <nightbladecodes@gmail.com>
2018-07-10 23:23:50 -0400

    Central signal manager. Signals work.

--- source/Scripts/SignalManager.gd
+++ source/Scripts/SignalManager.gd
@@ -1,18 +1,15 @@
 extends Node
 
+signal player_moved(to_position, facing)
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	load("res://Entities/Player.gd")
-	print("Auto-warp reportin'")
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	SignalManager.connect("player_moved", self, "_on_player_moved")
-
-func _on_player_moved(position, facing):
-	print("Player moved to " + str(position) + " and is facing " + facing)
+	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
